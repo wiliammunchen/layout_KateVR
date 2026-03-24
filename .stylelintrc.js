4d8f5f0b1c350c55c9b00e8c85b308f8eba4f7ce
wiliammunchen <wiliam.munchen@gmail.com>
2026-03-24 18:41:39 -0300

    feat: update KateVR responsive landing page with BEM methodology

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,11 +4,8 @@ module.exports = {
   rules: {
     'declaration-empty-line-before': null,
     'font-family-name-quotes': null,
-    'no-descending-specificity': null,
-    'number-max-precision': null,
     'order/order': null,
     'order/properties-order': null,
-    'scss/at-mixin-pattern': null,
     'scss/dollar-variable-empty-line-before': null,
   },
 };
