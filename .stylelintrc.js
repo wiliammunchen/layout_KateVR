f126aa32952bfa44767082698ce940808a906a48
wiliammunchen <wiliam.munchen@gmail.com>
2026-04-03 18:01:44 -0300

    fix: update responsive layout arc and stylelint errors

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,7 @@
 module.exports = {
-  extends: '@mate-academy/stylelint-config',
-  plugins: ['stylelint-scss'],
-  rules: {
-    'declaration-empty-line-before': null,
-    'font-family-name-quotes': null,
-    'order/order': null,
-    'order/properties-order': null,
-    'scss/dollar-variable-empty-line-before': null,
-  },
+  extends: "@mate-academy/stylelint-config",
+  plugins: [
+    "stylelint-scss"
+  ],
+  rules: {}
 };
